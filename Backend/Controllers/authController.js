c781c70e3505b4917623f11b0fbdef06d3191e19
krutagya <krutagya.kaneria.cg@gmail.com>
2025-02-04 07:22:02 +0530

    DoctorSchema is writen the way hospital can add doctor or doctor can login also import in the registraction part

--- Backend/Controllers/authController.js
+++ Backend/Controllers/authController.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
-
+import Doctor from "../models/DoctorSchema.js"
 
 export const register = async (req,res) => {
     const {email, password, name, role, photo, gender} = req.body
@@ -46,7 +46,7 @@ export const register = async (req,res) => {
         }
         await user.save()
         res.status(200).json({success:true, message:"User successfully created"})
-
+        
     } catch(err) {
         res.status(500).json({success:true, message:"Internal server error"})
     };
