60fe24d1b923dcf1fb891ff70c8d488e3a6df0bc
krutagya <krutagya.kaneria.cg@gmail.com>
2025-02-14 16:11:35 +0530

    Refactored Features section with a modern UI, improved responsiveness, added animations, and cleaned up code for better readability

--- Frountend/src/components/About/About.jsx
+++ Frountend/src/components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   return (
     <section>
         <div className='container'>
-            <div className='flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row mt-20'>
+            <div className='flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row'>
                 {/* about image */}
 
                 <div className='relative w-3/4 lg:-1/2 xl:w-[770px] z-10 order-2 lg:order-1 '>
