2216a29a4c1c371cc4dbc83d11017e53a46b1c24
krutagya <krutagya.kaneria.cg@gmail.com>
2025-02-14 18:52:46 +0530

    dubugging problem face in setup grid for doctor

--- Frountend/src/components/Doctors/DoctorsList.jsx
+++ Frountend/src/components/Doctors/DoctorsList.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {doctors} from '../../assets/data/doctors.js'
-import DoctorCard from './DoctorCard'
+import DoctorCard from './DoctorCard.jsx'
 
 const DoctorsList = () => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
       {doctors.map(doctor=>(
       <DoctorCard key={doctor.id} doctor={doctor} />))}
     </div>
