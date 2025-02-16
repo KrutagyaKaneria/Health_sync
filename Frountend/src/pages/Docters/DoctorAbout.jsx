8fdd666ff90deff38a813142b6546c169c82cb8e
krutagya <krutagya.kaneria.cg@gmail.com>
2025-02-17 00:22:46 +0530

    Added the DoctorAbout component to display detailed information about a doctor's background, including education history with formatted date representation. Implemented a formateDate utility function to ensure date consistency across the application. Enhanced UI with structured sections for readability and responsiveness. 📅🎓

---
+++ Frountend/src/pages/Docters/DoctorAbout.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { formateDate } from '../../utils/formateData.js'
+
+const DoctorAbout = () => {
+  return (
+    <div>
+    <div>
+      <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2'>
+        About of
+        <span>
+            jiten Panjwani
+        </span>
+      </h3>
+      <p className='text_para'>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    </div>
+
+
+    <div className='mt-12'>
+        <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>
+            Education
+        </h3>
+
+        <ul className='pt-4 md:p-5'>
+            <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+                <div>
+                    <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
+                    {formateDate("08-02-2024")}  -  {formateDate("10-02-2025")}
+                    </span>
+                    <p className='text-[16px] leading-6 font-medium text-textColor'>
+                        PHD in Surgeon
+                    </p>
+                </div>
+                <p className='text-[14px] leading-5 font-medium text-textColor'>
+                        New apollo Hospital, Delhi
+                </p>
+            </li>
+            <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+                <div>
+                    <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
+                        {formateDate("07-04-2010")}  -  {formateDate("12-07-2013")}
+                    </span>
+                    <p className='text-[16px] leading-6 font-medium text-textColor'>
+                        PHD in Surgeon
+                    </p>
+                </div>
+                <p className='text-[14px] leading-5 font-medium text-textColor'>
+                        New apollo Hospital, Delhi
+                </p>
+            </li>
+        </ul>
+    </div>
+    </div>
+  )
+}
+
+export default DoctorAbout
--- Frountend/src/pages/Docters/DoctorDetails.jsx
+++ Frountend/src/pages/Docters/DoctorDetails.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import doctorImg from "../../assets/images/doctor-img02.png"
 import starIcon from '../../assets/images/Star.png'
+import DoctorAbout from './DoctorAbout'
+import Feedback from './Feedback'
 
 const DoctorDetails = () => {
 
@@ -49,6 +51,14 @@ const DoctorDetails = () => {
         className={`${ tab === "feedback" && "border-b border-solid border-primaryColor"} py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}>Feedback</button>
       </div>
       
+      <div className='mt-[50px]'>
+        {
+          tab === "about" && <DoctorAbout/>
+        }
+        {
+          tab === "feedback" && <Feedback/>
+        }
+      </div>
 
 
     </div>
