0b275d4df890f268458080d2b6ab7197d95162e8
krutagya <krutagya.kaneria.cg@gmail.com>
2025-02-16 19:06:50 +0530

    Implemented the Services section with dynamic data rendering. Added Services, ServicesList, and ServicesCard components to display healthcare services. The components fetch data from a centralized services.js file and use responsive grid layouts for a clean UI. Each service card includes a title, description, and navigation button styled with dynamic colors. 🚀

--- Frountend/src/pages/Services.jsx
+++ Frountend/src/pages/Services.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { services } from '../assets/data/services.js'
+import ServicesCard from '../components/Services/ServicesCard'
 
 const Services = () => {
   return (
     <section>
-    <div>
+    <div className='container'>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] '>
+        {services.map((item,index) => (
+            <ServicesCard item={item} index={index} key={index}/>
+        ))}
       
+    </div>
     </div>
 
     </section>
@@ -12,4 +19,4 @@ const Services = () => {
   )
 }
 
-export default Services
+export default Services;
