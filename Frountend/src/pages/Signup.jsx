76837444530cdacdf9985338501606e20f313ef4
krutagya <krutagya.kaneria.cg@gmail.com>
2025-02-17 11:49:16 +0530

    🎨 Added Signup Component with Responsive Design and Image Section
    Implemented the Signup component layout.
    Integrated a signup animation (GIF) to enhance user engagement.
    Applied responsive design, ensuring adaptability across different screen sizes.
    Structured the UI with a grid layout, including an image section for larger screens.
    Styled the image container with a primary color background and rounded corners for a polished look. 🚀🎉

--- Frountend/src/pages/Signup.jsx
+++ Frountend/src/pages/Signup.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-
+import signupImg from "../assets/images/signup.gif";
 const Signup = () => {
   
-
   return (
-    <div>
-      
-    </div>
+    <section>
+      <div className='px-5 xl:px-0'>
+        <div className='max-w-[1170px] mx-auto'>
+          <div className='grid grid-cols-1 lg:grid-cols-2'>
+            {/* img box */}
+            <div className='hidden lg:block bg-primaryColor rounded-l-lg'>
+              <figure className='rounded-l-lg'>
+                <img src={signupImg} alt="" className='w-full rounded-l-lg' />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
