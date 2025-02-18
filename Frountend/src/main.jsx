39b7fd362ef2d55db88250c55e32bedd3fb16e0c
krutagya <krutagya.kaneria.cg@gmail.com>
2025-02-18 16:05:57 +0530

    🔒 Implemented AuthContext Provider for Authentication Management
    - Introduced  to encapsulate the authentication logic and provide the , , and  values across the app using context.
    - Integrated  with the  to manage authentication state changes (LOGIN_SUCCESS, LOGOUT).
    - Updated login form submission to dispatch login action with correct user data (, , ) to context after a successful login.
    - Wrapped the app in the  to ensure authentication state is available throughout the app.
    - Added error handling for login failures with toast notifications for improved user feedback.

--- Frountend/src/main.jsx
+++ Frountend/src/main.jsx
@@ -5,10 +5,12 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import  "react-toastify/dist/ReactToastify.css"
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthContextProvider>
     <App />
     <ToastContainer
     theme='dark'
@@ -17,6 +19,7 @@ ReactDom.createRoot(document.getElementById('root')).render(
     closeOnClick
     pauseOnHover={false}
      />
+     </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
