fbba7ea3acd87401eecbebdee2f5310e526c42d5
krutagya <krutagya.kaneria.cg@gmail.com>
2025-02-19 14:23:37 +0530

    add MyAccount component with user profile display

--- Frountend/src/Dashboard/user-account/MyAccount.jsx
+++ Frountend/src/Dashboard/user-account/MyAccount.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import userImg from '../../assets/images/doctor-img01.png'
 
 const MyAccount = () => {
   return (
-    <div>
-      <h1>krutagya</h1>
+    <div className='max-w-[1170px] px-5 mx-auto'>
+        <div className='grid md:grid-cols-3 gap-10'>
+            <div className='pb-[50px] px-[30px] rounded-md'>
+                <div className='flex items-center justify-center'>
+                    <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-x-primaryColor'>
+                        <img 
+                        src={userImg} 
+                        alt="" 
+                        className='w-full h-full rounded-full'/>
+                    </figure>
+                </div>
+
+                <div className='text-center mt-4'>
+                    <h3 className='text-[18px] leading-[30px] text-headingColor font-bold'>Jiten Panjvani</h3>
+                    <p className="text-textColor text-[15px] leading-6 font-medium">example@gmail.com</p>
+                </div>
+            </div>
+        </div>
+      
     </div>
   )
 }
