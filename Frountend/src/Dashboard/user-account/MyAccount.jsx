a366fa9fd5122689d250ad1b770e4ff1a5518808
krutagya <krutagya.kaneria.cg@gmail.com>
2025-02-19 14:34:04 +0530

    implementing logout,delete and bloodtype in the display

--- Frountend/src/Dashboard/user-account/MyAccount.jsx
+++ Frountend/src/Dashboard/user-account/MyAccount.jsx
@@ -7,7 +7,7 @@ const MyAccount = () => {
         <div className='grid md:grid-cols-3 gap-10'>
             <div className='pb-[50px] px-[30px] rounded-md'>
                 <div className='flex items-center justify-center'>
-                    <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-x-primaryColor'>
+                    <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor'>
                         <img 
                         src={userImg} 
                         alt="" 
@@ -18,6 +18,12 @@ const MyAccount = () => {
                 <div className='text-center mt-4'>
                     <h3 className='text-[18px] leading-[30px] text-headingColor font-bold'>Jiten Panjvani</h3>
                     <p className="text-textColor text-[15px] leading-6 font-medium">example@gmail.com</p>
+                    <p className="text-textColor text-[15px] leading-6 font-medium">Blood Type: <span className='ml-2 text-headingColor text-[22px] leading-8 '>B-</span></p>
+                </div>
+
+                <div className='mt-[50px] md:mt-[100px]'>
+                    <button className='w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white'>Logout</button>
+                    <button className='w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white'>Delete account</button>
                 </div>
             </div>
         </div>
